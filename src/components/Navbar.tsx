af7329b7b7cd938cb06a181ea9ca5968ffaefc2b
Phunsup S <avg.artvengers@gmail.com>
2024-05-29 09:51:02 +0700

    phunsup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,35 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Divider from '@mui/material/Divider';
 import { useUser } from './UserContext';
+import { IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
 
 export default function Navbar() {
   const { userId, displayName, pictureUrl, isLoggedIn, login, logout } = useUser();
+
+  const pages = ['Products', 'Pricing', 'Blog'];
+  const settings1 = ['Profile'];
+
+  const settings = [displayName] ;
+
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
 
   const handleTitleClick = () => {
     window.location.href = '/';
@@ -28,9 +54,38 @@ export default function Navbar() {
       </div>
       <AppBar position="static" sx={{ backgroundColor: 'black' }}>
         <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          {/* <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Avatar alt="Profile Picture" src={pictureUrl} sx={{ mr: 2 }} />
             <p>{displayName}</p>
+          </Box> */}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Profile Picture" src={pictureUrl}/>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
           <Box
             sx={{ position: 'absolute', left: '50%', transform: 'translateX(-50%)', whiteSpace: 'nowrap', cursor: 'pointer' }}
