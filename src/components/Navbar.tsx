93bf25920e2265c1818639717025c2dca5ff14a6
Phunsup S <avg.artvengers@gmail.com>
2024-11-15 00:53:49 +0700

    Change Name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -108,7 +108,7 @@ export default function Navbar() {
             onClick={handleTitleClick}
           >
             <Typography className='colorTypography' variant="h4" component="div">
-              Kwanpitcha Store
+              Phunsup Store
             </Typography>
           </Box>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
